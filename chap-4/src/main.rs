06c65251e1481e18642e21cbd2a7cf3a4dc095ad
valekar <srinivas.valekar@gmail.com>
2021-07-17 20:35:39 +0100

    chap-4

--- chap-4/src/main.rs
+++ chap-4/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    let y = 5;
+    let x = y;
+
+    println!("{} {}", x, y);
+
+    let mut s1 = String::from("hello world");
+    let s2 = &s1;
+    println!("{}", s2);
+
+    let s3 = &mut s1;
+    let s4 = &mut s1;
+
+    //println!("{}", s3);
+    println!("{}", s1);
+
+    //println!("{} {}", s1, s2);
 }
