1e494ffa36f98a0ba187395c037fab020b60dfb2
valekar <srinivas.valekar@gmail.com>
2021-07-17 14:13:30 +0100

    chap-3

--- chap-3/src/main.rs
+++ chap-3/src/main.rs
@@ -1,3 +1,34 @@
+// fn main() {
+//     //let y = x = 8;
+
+//     println!("Hello, world!");
+//     println!("{}", something_test());
+
+//     fn something_test() -> u8 {
+//         1
+//     }
+// }
+
+// fn main() {
+//     let x = plus_one(5);
+
+//     println!("{}", x);
+//     print!("{}", x.clone());
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("{}", result);
 }
